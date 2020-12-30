1ddbd6c75a8ccdb4c6588635c4253a23edd87577
Rougemoot <vermoot@gmail.com>
2020-12-30 17:02:30 +0100

    added steno and a couple things, doesn't compile as is though

--- keyboards/jj40/keymaps/Rougemoot/keymap.c
+++ keyboards/jj40/keymaps/Rougemoot/keymap.c
@@ -1,27 +1,17 @@
-/* Vermoot's ortho grid keymap */
-
-
-/* --------------- BLANK LAYOUT -------------- {{{
-
-[] = LAYOUT_ortho_4x12( \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
-)
-
----------------------------------------------- }}} */
+// Vermoot's ortho grid keymap
 
 // --------------- DEFINITIONS --------------- {{{
 
 // ---- Options ---- {{{
 
+// Mandatory
 #include QMK_KEYBOARD_H
 
-// Home row mods options
-#define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD
-#define TAPPING_TERM 160
+// Steno stuff
+#include "keymap_steno.h"
+void matrix_init_user() {
+    steno_set_mode(STENO_MODE_GEMINI);
+}
 
 // ----------------- }}}
 
@@ -34,7 +24,10 @@ enum layers {
   _NAV,
   _NUM,
   _MEDIA,
-  _FUNCT
+  _FUNCT,
+  _SELECT,
+  _STENO,
+  _GAMING
 };
 
 enum custom_keycodes {
@@ -81,21 +74,28 @@ bool accented_letter(uint16_t accent, uint16_t letter, bool pressed, uint8_t mod
 // ---- Custom keys ---- {{{
 
 // Modtaps
-#define ESC_FN MT(KC_APPLE_FN, KC_ESC)
+#define ESC_FN MT(MOD_HYPR, KC_ESC)
 #define TAB_MEH MEH_T(KC_TAB)
 #define SFT_ENT RSFT_T(KC_ENT)
 
 // Home row mods
-#define SFT_A MT(KC_LSFT, KC_A)
-#define SFT_SC MT(KC_RSFT, KC_SCLN)
+#define SFT_A MT(MOD_LSFT, KC_A)
+#define SFT_SC MT(MOD_RSFT, KC_SCLN)
+#define CTL_S MT(MOD_LCTL, KC_S)
+#define CTL_L MT(MOD_RCTL, KC_L)
+#define ALT_D MT(MOD_LALT, KC_D)
+#define ALT_K MT(MOD_LALT, KC_K)
+#define CMD_F MT(MOD_LGUI, KC_F)
+#define CMD_J MT(MOD_RGUI, KC_J)
 
 // Layer keys
+#define BASE  DF(_BASE) 
 #define RAISE MO(_RAISE) 
 #define LOWER MO(_LOWER) 
 #define NAV   MO(_NAV) 
 #define NUM   MO(_NUM) 
-#define MEDIA MO(_MEDIA)
-#define FUNCT  MO(_FUNCT) 
+#define MEDIA LT(_MEDIA, KC_ENT)
+#define FUNCT MO(_FUNCT) 
 
 // Accented letters
 #define C_CED   RALT(KC_C)
@@ -141,12 +141,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/* ---- BLANK LAYOUT ---- {{{
+
+// ----  ---- {{{
+[] = LAYOUT_ortho_4x12(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+)
+// ---------- }}}
+
+------------------------- }}} */
+
 // ---- BASE LAYER ---- {{{
 [_BASE] = LAYOUT_ortho_4x12( \
   TAB_MEH, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    MEDIA,
-  ESC_FN,  SFT_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SFT_SC,  UD_APO,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
-  KC_LCTL, KC_LALT, KC_LGUI, NUM,     NAV,     LOWER,   RAISE,   KC_SPC,  KC_BSPC, FUNCT,   _______, _______
+  ESC_FN,  SFT_A,   CTL_S,   ALT_D,   CMD_F,   KC_G,    KC_H,    CMD_J,   ALT_K,   CTL_L,   SFT_SC,  UD_APO,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  KC_LCTL, KC_LALT, KC_LGUI, NUM,     NAV,     LOWER,   RAISE,   KC_SPC,  KC_BSPC, FUNCT,   NK_TOGG, _______
 ),
 // -------------------- }}}
 
@@ -163,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ortho_4x12( \
   _______, _______, _______, E_GRV,   _______, _______, _______, U_GRV,   E_CIRC,  KC_MINS, KC_EQL,  _______,
   _______, KC_1,    KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS,
-  _______, UD_TLD,  _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,
+  _______, UD_TLD,  _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 // --------------- }}}
@@ -171,7 +184,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ---- NAV ---- {{{
 [_NAV] = LAYOUT_ortho_4x12( \
   _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,
-  _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_ENT,
+  _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______
 ),
@@ -196,13 +209,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // --------------- }}}
 
 // ---- FUNCT ---- {{{
-[_FUNCT] = LAYOUT_ortho_4x12( \
-  KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  KC_F7,   KC_F8,   KC_F9,   _______, _______, _______, _______, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT, _______, \
-  KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  KC_F1,   KC_F2,   KC_F3,   _______, _______, _______, _______, _______, _______, _______, _______, _______ \
-)
+[_FUNCT] = LAYOUT_ortho_4x12(
+  KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_F7,   KC_F8,   KC_F9,   _______, _______, _______, _______, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT, _______,
+  KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_F1,   KC_F2,   KC_F3,   _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
 // --------------- }}}
+
+// ---- SELECT ---- {{{
+[_SELECT] = LAYOUT_ortho_4x12(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DF(_STENO),
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+// ---------------- }}}
+
+// ---- STENO ---- {{{
+[_STENO] = LAYOUT_ortho_4x12(
+  XXXXXXX, XXXXXXX, STN_N3,  STN_N4,  STN_N5,  XXXXXXX, XXXXXXX, STN_N8,  STN_N9,  STN_NA,  XXXXXXX, XXXXXXX,
+  XXXXXXX, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
+  XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, STN_A,   STN_O,   STN_E,   STN_U,   XXXXXXX, XXXXXXX, XXXXXXX, BASE
+)
+// ---------------- }}}
 
 };
 
