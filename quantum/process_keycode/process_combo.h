250759c2ca15c181c07b4800f9f0966112e3cb64
Vermoot <vermoot@gmail.com>
2021-06-06 15:02:06 +0200

    fix the bad process_combo.h

--- quantum/process_keycode/process_combo.h
+++ quantum/process_keycode/process_combo.h
@@ -21,9 +21,6 @@
 #include <stdint.h>
 
 #if defined(EXTRA_EXTRA_LONG_COMBOS)
-#ifdef EXTRA_SHORT_COMBOS
-#    define MAX_COMBO_LENGTH 6
-#elif defined(EXTRA_EXTRA_LONG_COMBOS)
 #    define MAX_COMBO_LENGTH 32
 #elif defined(EXTRA_LONG_COMBOS)
 #    define MAX_COMBO_LENGTH 16
@@ -46,18 +43,9 @@ typedef struct {
 #if defined(EXTRA_EXTRA_LONG_COMBOS)
     uint32_t state;
 #elif defined(EXTRA_LONG_COMBOS)
-#ifdef EXTRA_SHORT_COMBOS
-    uint8_t state;
-#else
-    bool disabled;
-    bool active;
-#    if defined(EXTRA_EXTRA_LONG_COMBOS)
-    uint32_t state;
-#    elif defined(EXTRA_LONG_COMBOS)
     uint16_t state;
-#    else
+#else
     uint8_t state;
-#    endif
 #endif
 } combo_t;
 
@@ -76,12 +64,6 @@ typedef struct {
 
 /* check if keycode is only modifiers */
 #define KEYCODE_IS_MOD(code) (IS_MOD(code) || ((code & 0xFF00) >= QK_MODS && (code & 0xFF00) <= QK_MODS_MAX && !(code & 0xFF)))
-#ifndef COMBO_HOLD_TERM
-#    define COMBO_HOLD_TERM TAPPING_TERM
-#endif
-
-/* check if keycode is only modifiers */
-#define KEYCODE_IS_MOD(code) (IS_MOD(code) || (code >= QK_MODS && code <= QK_MODS_MAX && !(code & QK_BASIC_MAX)))
 
 bool process_combo(uint16_t keycode, keyrecord_t *record);
 void matrix_scan_combo(void);
